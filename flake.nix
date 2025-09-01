7662f15cf8585ed0a9cf4b6c2c44b6346a175cb9
cglavin50 <cooperglavin@gmail.com>
2025-09-01 19:43:18 -0400

    fix: refactor actions to use flake for fonts

--- flake.nix
+++ flake.nix
@@ -14,12 +14,24 @@
     flake-utils.lib.eachDefaultSystem (system: let
       # grab nixpkgs for our system
       pkgs = nixpkgs.legacyPackages.${system};
+
+      # Installing fonts - see https://nix.dev/guides/recipes/continuous-integration-github-actions
+      fontsConf = pkgs.makeFontsConf {
+        fontDirectories = [
+          pkgs.nerd-fonts.droid-sans-mono
+        ];
+      };
     in {
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           typst
           # tinymist
+          nerd-fonts.droid-sans-mono
         ];
+
+        shellHook = ''
+          export FONTCONFIG_FILE="${fontsConf}"
+        '';
       };
     });
 }
